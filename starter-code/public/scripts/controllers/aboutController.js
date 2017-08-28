644b766c8264df7ba5c4b4f8dad737af182bdd95
mackoyokcam <mark.hangarter@gmail.com>
2017-08-28 13:14:45 -0700

    finished routing scripts

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -5,8 +5,10 @@ var app = app || {};
   const aboutController = {};
 
   // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
-  $('.tab-content').hide();
-  $('#about').fadeIn();
+  aboutController.init = function() {
+    $('.tab-content').hide();
+    $('#about').fadeIn();
+  }
 
   module.aboutController = aboutController;
 })(app);
